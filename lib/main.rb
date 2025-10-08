0ba050c66921f3190002fb99dfbf1f188e7d7ef8
KevinS <kevin.saison91@gmail.com>
2025-10-08 14:00:58 +0200

    Implementation of a basic parser in the main program with OptionParser

---
+++ lib/main.rb
@@ -0,0 +1,13 @@
+require 'optparse'
+
+if __FILE__ == $0
+  options = {}
+
+  parser = OptionParser.new do |opts|
+    opts.banner = 'Usage: ruby main.rb [brainfuck_file]'
+    opts.on('-h', '--help', 'Show this help message') do
+      puts opts
+      exit
+    end
+  end
+end

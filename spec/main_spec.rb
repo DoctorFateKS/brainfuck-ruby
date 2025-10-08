295584c9a6f05ad53269a7a44a6a77e864054557
KevinS <kevin.saison91@gmail.com>
2025-10-08 12:50:09 +0200

    Correction of style with Rubocop suggestions

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
+
 require 'open3'
 
-RSpec.describe "main program" do
-    it "prints the correct output from a simple brainfuck file" do
-        stdout, stderr, status = Open3.capture3("ruby", "lib/main.rb", "hello_world.bf")
-        expect(stdout).to eq("Hello World!")
-    end
+RSpec.describe 'main program' do
+  it 'prints the correct output from a simple brainfuck file' do
+    stdout, _stderr, _status = Open3.capture3('ruby', 'lib/main.rb', 'hello_world.bf')
+    expect(stdout).to eq('Hello World!')
+  end
 end

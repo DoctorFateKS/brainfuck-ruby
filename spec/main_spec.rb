fe97b4aebc1c3c16aef8ceca96f4e955d0343fd6
KevinS <kevin.saison91@gmail.com>
2025-10-08 11:37:58 +0200

    Implementation of Rspec test to verify a correct output

---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+require 'open3'
+
+RSpec.describe "main program" do
+    it "prints the correct output from a simple brainfuck file" do
+        stdout, stderr, status = Open3.capture3("ruby", "lib/main.rb", "hello_world.bf")
+        expect(stdout).to eq("Hello World!")
+    end
+end

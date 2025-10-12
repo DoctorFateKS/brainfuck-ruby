bb5b04187f55b7e2680b186b71fc9b2e1d71009f
KevinS <kevin.saison91@gmail.com>
2025-10-12 19:10:13 +0200

    Implementation of basic errors handling

--- lib/main.rb
+++ lib/main.rb
@@ -11,5 +11,13 @@ if __FILE__ == $PROGRAM_NAME
   end.parse!(into: options)
 
   brainfuck_file = ARGV.first
+  if brainfuck_file.nil?
+    puts 'Please specify a brainfuck file'
+    exit 1
+  elsif !File.exist?(brainfuck_file)
+    puts "The file #{brainfuck_file} doesn't exists"
+    exit 1
+  end
+
   Brainfuck.new(brainfuck_file).execute
 end

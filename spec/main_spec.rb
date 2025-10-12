6b858edab1011e9d013cff30700cf85dc2094d13
KevinS <kevin.saison91@gmail.com>
2025-10-12 18:39:44 +0200

    Implementation of integration tests for each examples

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,8 +3,18 @@
 require 'open3'
 
 RSpec.describe 'main program' do
-  it 'prints the correct output from a simple brainfuck file' do
-    stdout, _stderr, _status = Open3.capture3('ruby', 'lib/main.rb', 'hello_world.bf')
-    expect(stdout).to eq('Hello World!')
+  it 'Prints the correct output for the hello world brainfuck example' do
+    stdout, _stderr, _status = Open3.capture3('ruby', 'lib/main.rb', 'Examples/hello_world.bf')
+    expect(stdout).to eq("Hello World!\n")
+  end
+  
+  it 'Prints the correct output for the fibonacci brainfuck example' do
+    stdout, _stderr, _status = Open3.capture3('ruby', 'lib/main.rb', 'Examples/fibonacci.bf')
+    expect(stdout).to eq("1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89")
+  end
+
+  it 'Prints the correct output for the cell_size brainfuck example' do
+    stdout, _stderr, _status = Open3.capture3('ruby', 'lib/main.rb', 'Examples/cell_size.bf')
+    expect(stdout).to eq("8 bit cells\n")
   end
 end
